b98ae5e5c221fd19250fbd8be328fdd345992ca3
Stefan Scarlat <stefanel.scarlat@gmail.com>
2026-05-27 20:00:58 +0300

    first commit

---
+++ Services/PointService.cs
@@ -0,0 +1,21 @@
+﻿using Recapitulare;
+
+namespace Recapitulare.Services
+{
+    internal class PointService
+    {
+        List<Punct>
+        public void CreatePoint(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+
+        }
+
+        public void UpdatePoint(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
